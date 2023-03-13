c839979cf1f6f9d76d8cf53bdc3c4dbb83fde67e
Milton Cavalcante <milton.hneto@gmail.com>
2023-03-13 09:33:08 -0300

    Criado componente Login - Fixed #30

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { NavigationComponent } from './components/navigation/navigation.componen
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TechnicianListComponent } from './components/technician/technician-list/technician-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { TechnicianListComponent } from './components/technician/technician-list
     NavigationComponent,
     HomeComponent,
     HeaderComponent,
-    TechnicianListComponent
+    TechnicianListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
