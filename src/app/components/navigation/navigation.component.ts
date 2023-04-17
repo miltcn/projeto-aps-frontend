4574d23572034b9e43df4ace55c7c7c9ae017b5b
Milton Cavalcante <milton.hneto@gmail.com>
2023-04-17 08:16:34 -0300

    Componente service-order-read finalizado

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -16,7 +16,7 @@ export class NavigationComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.router.navigate(['chamados/read/1']);
+    this.router.navigate(['home']);
   }
 
   logout(): void {
--- src/app/components/serviceOrder/service-order-read/service-order-read.component.ts
+++ src/app/components/serviceOrder/service-order-read/service-order-read.component.ts
@@ -1,15 +1,66 @@
+import { ServiceOrder } from './../../../models/serviceOrder';
+import { ServiceOrderService } from 'src/app/services/service-order.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Client } from 'src/app/models/client';
+import { Technician } from 'src/app/models/technician';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-service-order-read',
   templateUrl: './service-order-read.component.html',
-  styleUrls: ['./service-order-read.component.css']
+  styleUrls: ['./service-order-read.component.css'],
 })
 export class ServiceOrderReadComponent implements OnInit {
+  serviceOrder: ServiceOrder = {
+    title: '',
+    statusCode: undefined,
+    priorityCode: undefined,
+    comments: '',
+    technicianId: '',
+    clientId: '',
+  };
 
-  constructor() { }
+  constructor(
+    private serviceOrderService: ServiceOrderService,
+    private toastrService: ToastrService,
+    private activatedRoute: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
+    this.serviceOrder.id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.getServiceOrder();
   }
 
+  getServiceOrder() {
+    this.serviceOrderService.findById(this.serviceOrder.id).subscribe(
+      response => {
+        this.serviceOrder = response;
+      },
+      err => {
+        this.toastrService.error(err.error.error);
+      },
+    );
+  }
+
+  getStatus(status: number) {
+    if (status === 0) {
+      return 'ABERTO';
+    } else if (status === 1) {
+      return 'ANDAMENTO';
+    } else {
+      return 'ENCERRADO';
+    }
+  }
+
+  getPriority(priority: number) {
+    if (priority === 0) {
+      return 'BAIXA';
+    } else if (priority === 1) {
+      return 'MÉDIA';
+    } else {
+      return 'ALTA';
+    }
+  }
 }
