52051f72ee2bc4bc89a9ca529899b391dfb5baa4
Milton Cavalcante <milton.hneto@gmail.com>
2023-04-16 23:46:22 -0300

    Implementado validação de formulário para service-order-create

--- src/app/components/serviceOrder/service-order-create/service-order-create.component.ts
+++ src/app/components/serviceOrder/service-order-create/service-order-create.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-service-order-create',
   templateUrl: './service-order-create.component.html',
-  styleUrls: ['./service-order-create.component.css']
+  styleUrls: ['./service-order-create.component.css'],
 })
 export class ServiceOrderCreateComponent implements OnInit {
+  title: FormControl = new FormControl(null, [Validators.required]);
+  status: FormControl = new FormControl(null, [Validators.required]);
+  priority: FormControl = new FormControl(null, [Validators.required]);
+  technician: FormControl = new FormControl(null, [Validators.required]);
+  client: FormControl = new FormControl(null, [Validators.required]);
+  comments: FormControl = new FormControl(null, [Validators.required]);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  validateFields(): boolean {
+    return (
+      this.title.valid &&
+      this.status.valid &&
+      this.priority.valid &&
+      this.technician.valid &&
+      this.client.valid &&
+      this.comments.valid
+    );
   }
-
 }
